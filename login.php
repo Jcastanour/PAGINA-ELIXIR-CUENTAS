6da732ab82b7f200126e1ea6f9fb8700f8a3c1e4
Juan Pablo C <jcastanour@unal.edu.co>
2024-07-16 17:55:55 -0500

    href

--- login.php
+++ login.php
@@ -10,7 +10,7 @@
       href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
       rel="stylesheet"
     />
-    <link rel="stylesheet" type="text/css" href="src/css/styles.css" />
+    <link rel="stylesheet" href="src/css/styles.css" />
   </head>
   <body>
     <div class="wrapper">
@@ -20,12 +20,10 @@
       </header>
       <div class="contenedor-formulario">
         <form action="" method="POST">
-
           <?php
           include "modelo/conexion.php";
           include "controladores/controlador_login.php"
           ?>
-          
           <p>Usuario: <br><input type="text" placeholder="Ingrese usuario" name="usuario"></p>
           <p>Contraseña: <br><input type="password" placeholder="Ingrese contraseña" name="contrasena"></p>
           <input type="submit" class="button" value="Ingresar" name = "btningresar">
