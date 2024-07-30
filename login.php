2dcd526fb0b33d47d3f04feac4b0abfc62e0cf69
Juan Pablo C <jcastanour@unal.edu.co>
2024-07-29 21:25:21 -0500

    rand version js y css

--- login.php
+++ login.php
@@ -17,7 +17,7 @@ if (!empty($_SESSION['id'])) {
   <link rel="preconnect" href="https://fonts.googleapis.com" />
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
   <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
-  <link rel="stylesheet" href="src/css/styles.css?v=1.1" />
+  <link rel="stylesheet" href="src/css/styles.css??v=<?php echo (rand()); ?>" />
 </head>
 
 <body>
@@ -41,7 +41,7 @@ if (!empty($_SESSION['id'])) {
   </div>
   </div>
 
-  <script src="src/js/scriptelixir.js"></script>
+  <script src="src/js/scriptelixir.js?v=<?php echo (rand()); ?>"></script>
 </body>
 
 </html>
