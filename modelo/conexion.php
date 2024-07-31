d42132be072e545f07cf52b43994caf1bd795231
Juan Pablo C <jcastanour@unal.edu.co>
2024-07-31 11:49:58 -0500

    Conexion arreglo

--- modelo/conexion.php
+++ modelo/conexion.php
@@ -1,5 +1,5 @@
 <?php
 
-// $conexion = new mysqli('127.0.0.1:3306', 'u948886594_elixir', 'Elixir1021.', 'u948886594_usuarios');
-$conexion = new mysqli('localhost', 'root', '', 'usuarios');
+$conexion = new mysqli('127.0.0.1:3306', 'u948886594_elixir', 'Elixir1021.', 'u948886594_usuarios');
+// $conexion = new mysqli('localhost', 'root', '', 'usuarios');
 $conexion->set_charset("utf8");
